287fdaea9b0c05669811d18407d83694e448afad
Przemek Kozinski <przemekoz@o2.pl>
2018-06-15 21:21:33 +0200

    dialog component test

--- src/app/payments/payment-detail-dialog/payment-detail-dialog.component.spec.ts
+++ src/app/payments/payment-detail-dialog/payment-detail-dialog.component.spec.ts
@@ -1,25 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PaymentDetailDialogComponent } from './payment-detail-dialog.component';
 
 describe('PaymentDetailDialogComponent', () => {
-  let component: PaymentDetailDialogComponent;
-  let fixture: ComponentFixture<PaymentDetailDialogComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PaymentDetailDialogComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PaymentDetailDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        const component = new PaymentDetailDialogComponent({ data: '' });
+        expect(component).toBeTruthy();
+    });
 });
